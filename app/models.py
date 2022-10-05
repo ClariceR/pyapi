bac38ea026716bbe43d378ba101ccc3b6674b3c4
cladev <cladev@macbook-air.mynet>
2022-10-05 14:52:25 +0100

    Add User model

--- app/models.py
+++ app/models.py
@@ -7,6 +7,7 @@ from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy import Column, Integer, String, Boolean
 from .database import Base
 
+# This is our Sqlalchemy ORM models, to define how our tables in Postgres will be
 class Post(Base):
     __tablename__ = "posts"
 
@@ -14,4 +15,14 @@ class Post(Base):
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     published = Column(Boolean, server_default='TRUE', nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
