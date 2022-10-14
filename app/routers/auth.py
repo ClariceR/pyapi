82aee6543df1f32b7a3f273387cd4402a813ddb9
cladev <cladev@macbook-air.mynet>
2022-10-14 11:53:32 +0100

    Refactored user credentials to use oauth password request form

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,6 +1,7 @@
 from os import access
 from pyexpat import model
 from fastapi import status, HTTPException, Depends, APIRouter
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from .. import models, schemas, utils, oauth2
@@ -14,8 +15,9 @@ router = APIRouter(
 )
 
 @router.post('/')
-def login(user_credentials: schemas.UserLogin, db: Session = Depends(database.get_db)):
-    user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
+    
+    user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
 
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
